bd61e4ffa08a8b835a28850f321f1ddf616055cb
mourik <hakim.mourik>
2016-07-25 14:31:57 +0200

    Add new Linked list Add comments

--- FccLtlTest/src/TestSg/KataTest.java
+++ FccLtlTest/src/TestSg/KataTest.java
@@ -1,4 +1,4 @@
-// amourik 24/07/2016
+// amourik040416, 24/07/2016
 // Technical test for ITEC/FCC/LTL - SGCIB
 
 package TestSg;
@@ -8,9 +8,9 @@ import java.util.LinkedList;
 public class KataTest {
 
        public static void main(String[] args) {
-
-              for (int i = 1; i <= 100; i++) {
-                     String res = "";
+    	   
+              for (int i = 1; i <= 100; i++) {            	  	
+                     String res = "";// res: variable où le resultat sera stocké 
                      if ((i % 3) == 0) {
                          res = res + "Foo";
                      }                   
@@ -23,15 +23,15 @@ public class KataTest {
                          res = res + "Qix";
                      }
                   
-                     int number = i; // = and int
-                     LinkedList<Integer> stack = new LinkedList<Integer>();
+                     int number = i;
+                     LinkedList<Integer> list = new LinkedList<Integer>(); // Création d'une Linked list sous forme de pile
+                     //conversion chaque nombre en list de digits. ex 37 --> 3;7 pour vérifier si le nombre contient (3, 5 ou 7) 
                      while (number > 0) {
-                        stack.push( number % 10  ); // insertion du reste de la division par 10
-                        number = number / 10; // diviser le numéro par 10
+                        list.push( number % 10  ); //insertion de l'élément dans la liste 
+                        number = number / 10;
                      }
-                     //
-                     while (!stack.isEmpty()) { //la methode qui verifie si le stack est vide ou pas
-                        int digit = stack.pop(); //
+                     while (!list.isEmpty()) { // la méthode qui vérifie si la liste contient un élément
+                        int digit = list.pop(); //supprime l'élément de la liste et renvoie le premier de cette liste pour le tester
 
                         if(digit == 3){
                             res = res +"Foo";
@@ -45,7 +45,7 @@ public class KataTest {
                             res = res +"Qix";
                         }
                      }
-
+                     //Affichage des nombres si "res" ne prend aucune valeur
                      if(res.length() == 0){
                            System.out.println(i);
                      }
